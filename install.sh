fd197ecd48e67fd7fbd0abb100d56e03805be4cf
Teagan Glenn <tglenn@infinite-improbability.int.teagantotally.rocks>
2018-06-23 15:02:28 -0600

    Setup homebrew

--- install.sh
+++ install.sh
@@ -5,3 +5,12 @@ cp -r .zsh/ ~/.zsh/
 cp -r .vim/ ~/.vim/
 cp .zshrc ~
 cp .vimrc ~
+
+printf "Installing HomeBrew"
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap Caskroom/cask
+
+printf "Installing Binaries"
+brew cask install google-chrome
